3d9dbc0748879c63c49d1076408373a402a81495
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-04-04 09:49:23 +0300

    udate

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 //! # Parsenmap
-//!
+//! All documents should be in XML file
+// It translates XML filr format data into csv or json
 //! `parsenmap` is a tool for parsing nmap xml file to csv or json.
 #[macro_use]
 mod macroses;
