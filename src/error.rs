68edac9b338885d81d1df55a940e56f6db017938
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-04-12 12:10:47 +0300

    log data

--- src/error.rs
+++ src/error.rs
@@ -7,7 +7,7 @@ pub struct ParsenmapError {
 impl From<xml::reader::Error> for ParsenmapError {
     ///
     /// Generates error from `xml::reader::Error`
-    //
+
     /// ```
 
     fn from(e: xml::reader::Error) -> Self {
