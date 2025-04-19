ba0cf1ca5b5c171ea5d46a430fe5df1d04701d73
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-04-19 11:07:28 +0300

    error logging

--- src/error.rs
+++ src/error.rs
@@ -18,8 +18,7 @@ impl From<std::io::Error> for ParsenmapError {
     //
     //
     //
-    //
-    
+    /
     /// Generates error from `std::io::Error`
     fn from(e: std::io::Error) -> Self {
         ParsenmapError { err: e.to_string() }
