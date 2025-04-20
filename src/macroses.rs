a5ad68072b1e0cb57256b10ecd0a6ca3cb4947a9
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-04-20 11:11:51 +0300

    macroses

--- src/macroses.rs
+++ src/macroses.rs
@@ -4,7 +4,7 @@ pub type SameType<T> = T;
 // pg 1
 // xrot
 // brain rot]
-// hdtoad.lll
+// hdtoad.llm
 #[macro_export]
 macro_rules! Initializer {
     (#[derive($($derive:meta),*)] $pub:vis struct $name:ident { $($fpub:vis $field:ident : $type:ty,)* }) => {
